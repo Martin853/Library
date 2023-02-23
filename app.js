309e12bd5592ef276efb3ca83ecbd47e8cc6951e
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-23 22:38:05 +0100

    Created the modal interface

---
+++ app.js
@@ -0,0 +1,6 @@
+const addBook = document.querySelector("#add-book");
+const modal = document.querySelector("#modal");
+
+addBook.addEventListener("click", function () {
+  modal.style.display = "block";
+});
